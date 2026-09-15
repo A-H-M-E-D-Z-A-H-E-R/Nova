20e9558d0cc29eff5ae13904b1bc49c8428627ea
medo <medoseo@outlook.com>
2026-09-15 20:49:33 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-output: "export",
+  output: "export",
   reactCompiler: true,
   devIndicators: false,
+
+  images: {
+    unoptimized: true,
+  },
 
   allowedDevOrigins: ["192.168.100.2"],
 };
