5ec354d4d9c1fb701ee78b6ca405372bc8cb9a69
medo <medoseo@outlook.com>
2026-09-15 20:57:55 +0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Nova",
-  assetPrefix: "/Nova/",
   reactCompiler: true,
   devIndicators: false,
 
